944fe491191e21fc0a8cd7c3911053f16974bd51
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-21 16:16:36 -0300

    Ejercicio 4.11 completado

--- workspace/4- Control de flujos if/src/ar/com/gl/flujos/Ejercicio411.java
+++ workspace/4- Control de flujos if/src/ar/com/gl/flujos/Ejercicio411.java
@@ -1,5 +1,15 @@
 package ar.com.gl.flujos;
 
 public class Ejercicio411 {
+	public static void main(String[] args) {
+		
+		String titulo = "BootCAmp 2020";
+		
+		char[] chars = titulo.toCharArray();
+		
+		for (char i : chars) {
+			System.out.println("Letra: " + i);
+		}
+	}
 
 }
