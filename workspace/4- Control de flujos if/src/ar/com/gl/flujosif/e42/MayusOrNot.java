0f03aa36298e5f65a01769aded92949bb75c932d
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-21 15:29:57 -0300

    Ejercicio 4.2 completado

---
+++ workspace/4- Control de flujos if/src/ar/com/gl/flujosif/e42/MayusOrNot.java
@@ -0,0 +1,18 @@
+package ar.com.gl.flujosif.e42;
+
+public class MayusOrNot {
+
+	public static void main(String[] args) {
+		
+		String letra = "A";
+		String letra2 = "b";
+		
+		if (letra2.toUpperCase() == letra2) {
+			System.out.println("Es una letra mayuscula");
+		} else {
+			System.out.println("No es una letra mayuscula");
+		}
+
+	}
+
+}
