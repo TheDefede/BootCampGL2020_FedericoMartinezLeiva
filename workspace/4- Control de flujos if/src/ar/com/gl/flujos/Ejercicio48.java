7c375243666078d3413565b704524a7147148f26
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-21 15:56:50 -0300

    Ejercicio 4.8 completado

---
+++ workspace/4- Control de flujos if/src/ar/com/gl/flujos/Ejercicio48.java
@@ -0,0 +1,20 @@
+package ar.com.gl.flujos;
+
+public class Ejercicio48 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while (i <= 50) {
+			if (i % 2 == 0) {
+				System.out.println("Valor iterado " + i + " es par");
+			}else {
+				System.out.println("Valor iterado " + i + " es impar");
+			}
+			i++;
+		}
+
+	}
+
+}
