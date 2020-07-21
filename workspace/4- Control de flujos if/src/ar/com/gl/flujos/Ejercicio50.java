10d614ecf0b588a8c60d012c852e9e301d5554a4
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-21 16:00:46 -0300

    Ejercicio 4.10 completado

---
+++ workspace/4- Control de flujos if/src/ar/com/gl/flujos/Ejercicio50.java
@@ -0,0 +1,14 @@
+package ar.com.gl.flujos;
+
+public class Ejercicio50 {
+	public static void main(String[] args) {
+		
+		String[] semana = {"Lunes", "Martes", 
+				"Miercoles" ,"Jueves", "Viernes" , "Sabado" , "Domingo"};
+
+		for (String i : semana) {
+			System.out.println("Dia de la semana: " + i);
+		}
+	}
+
+}
