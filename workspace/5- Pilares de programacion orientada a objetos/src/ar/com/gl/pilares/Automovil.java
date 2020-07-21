42c2991f7ca4cb0a7f5da17402071a6117cf4ced
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-21 19:17:56 -0300

    Ejercicio 5.4 completado

---
+++ workspace/5- Pilares de programacion orientada a objetos/src/ar/com/gl/pilares/Automovil.java
@@ -0,0 +1,11 @@
+package ar.com.gl.pilares;
+
+abstract class Automovil {
+	
+	abstract void retroceder();
+	
+	void avanzar() {
+		System.out.println("Soy el metodo avanzar de Automovil");
+	}
+
+}
---
+++ workspace/5- Pilares de programacion orientada a objetos/src/ar/com/gl/pilares/Berlina.java
@@ -0,0 +1,19 @@
+package ar.com.gl.pilares;
+
+public class Berlina extends Automovil {
+
+	@Override
+	void retroceder() {
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+		Berlina berlina = new Berlina();
+		
+		berlina.avanzar();
+
+	}
+
+}
