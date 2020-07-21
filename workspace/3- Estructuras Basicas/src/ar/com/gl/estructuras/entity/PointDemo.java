5da624d69f84fdea9eb37b78e998fbb7e9417731
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-21 15:08:22 -0300

    Ejercicio 3.5 completado

--- workspace/3- Estructuras Basicas/src/ar/com/gl/estructuras/entity/PointDemo.java
+++ workspace/3- Estructuras Basicas/src/ar/com/gl/estructuras/entity/PointDemo.java
@@ -4,9 +4,12 @@ public class PointDemo {
 
 	public static void main(String[] args) {
 		
-		VariableInstancia vl = new VariableInstancia();
+		VariableInstancia vI = new VariableInstancia();
 		
+		vI.engPoints = 50;
+		vI.mathsPoints = 80;
 		
+		System.out.println("El valor de engPoints es: " + vI.engPoints + " y el de mathsPoints es: " + vI.mathsPoints);
 
 	}
 
--- workspace/3- Estructuras Basicas/src/ar/com/gl/estructuras/entity/VariableInstancia.java
+++ workspace/3- Estructuras Basicas/src/ar/com/gl/estructuras/entity/VariableInstancia.java
@@ -1,14 +1,9 @@
 package ar.com.gl.estructuras.entity;
 
-public class VariableInstancia {
-
-	public static void main(String[] args) {
-		
-		int engPoints;
-		int mathsPoints;
-		int phyPoints;
-		
-
-	}
+public class VariableInstancia {	
+	
+	int engPoints;
+	int mathsPoints;
+	int phyPoints;
 
 }
