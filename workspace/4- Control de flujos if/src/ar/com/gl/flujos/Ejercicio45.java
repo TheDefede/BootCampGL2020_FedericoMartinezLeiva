06cc8ea21f3048b1e8658323b6d930e1c2d71a46
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-21 15:49:33 -0300

    Ejercicio 4.5 completado

---
+++ workspace/4- Control de flujos if/src/ar/com/gl/flujos/Ejercicio45.java
@@ -0,0 +1,20 @@
+package ar.com.gl.flujos;
+
+public class Ejercicio45 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 10000; i++) {
+			if (i % 20 == 0) {
+				i++;
+
+				System.out.println(i);
+			}
+		}
+
+		
+	}
+	
+	
+
+}
