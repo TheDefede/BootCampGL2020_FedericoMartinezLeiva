c790bdb6705f050dbb9c5a65f46266931b5b6837
TheDefede <62037106+TheDefede@users.noreply.github.com>
2020-07-21 14:42:48 -0300

    ejercicio 3.4

---
+++ workspace/3- Estructuras Basicas/src/ar/com/gl/estructuras/entity/VariableLocal.java
@@ -0,0 +1,20 @@
+package ar.com.gl.estructuras.entity;
+
+public class VariableLocal {
+
+	public static void main(String[] args) {
+		
+		VariableLocal estudiante = new VariableLocal();
+		
+		estudiante.studentAge();
+
+	}
+	
+	public void studentAge() {
+		int age = 0;
+		age +=5;
+		
+		System.out.println();	
+	}
+
+}
