8f4f188451561904509bc8171f9c8c46a8ae0e64
thedefede <fedemartinezleiva@gmail.com>
2020-08-03 11:21:06 -0300

    organizacion carpetas

--- workspace/3- Estructuras Basicas/src/ar/com/gl/estructuras/entity/Empleado.java
+++
@@ -1,8 +0,0 @@
-package ar.com.gl.estructuras.entity;
-
-public class Empleado {
-	
-	static double salario;
-	static String nombre = "Alex";
-
-}
--- workspace/3- Estructuras Basicas/src/ar/com/gl/estructuras/entity/VariableInstancia.java
+++
@@ -1,9 +0,0 @@
-package ar.com.gl.estructuras.entity;
-
-public class VariableInstancia {	
-	
-	int engPoints;
-	int mathsPoints;
-	int phyPoints;
-
-}
